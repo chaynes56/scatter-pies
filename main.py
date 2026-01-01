6282a7113256070eef04a5729dac00c1e6d9485b
Chris Haynes <75630580+chaynes56@users.noreply.github.com>
2025-12-31 23:42:51 -0600

    clean up main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Show demo scatter plot of pie charts
+# From https://stackoverflow.com/questions/66512564/matplotlib-pie-charts-as-scatter-plot#:~:\
+# text=def%20plot_pie%28x%2C%20ax%2C,%29%20ax.set_frame_on%28True%29
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -10,30 +9,27 @@ import random
 def rand(): #simulate some random data
     return [random.randint(0,100) for _ in range(10)]
 
+# from https://stackoverflow.com/questions/66512564/matplotlib-pie-charts-as-scatter-plot#:~:text=def%20plot_pie%28x
+# %2C%20ax%2C,%29%20ax.set_frame_on%28True%29
 def plot_pie(x, ax):
     ax.pie(x[['a', 'b', 'c']], center=(x['lat'], x['lon']), radius=1, colors=['r', 'b', 'g'],
            wedgeprops={'clip_on': True}, frame=True)
 
-# my data is stored in a similar styled dataframe that I read from a csv and the data is static
-sim_data = pd.DataFrame({'a':rand(),'b':rand(),'c':rand(), 'lat':rand(),'lon':rand()})
+def scatter_pie_plots():
+    # my data is stored in a similar styled dataframe that I read from a csv and the data is static
+    sim_data = pd.DataFrame({'a':rand(),'b':rand(),'c':rand(), 'lat':rand(),'lon':rand()})
 
-fig, ax = plt.subplots()
-plt.scatter(x=sim_data['lat'], y=sim_data['lon'], s=1000, facecolor='none',edgecolors='r')
-y_init = ax.get_ylim()
-x_init = ax.get_xlim()
+    fig, ax = plt.subplots()
+    plt.scatter(x=sim_data['lat'], y=sim_data['lon'], s=1000, facecolor='none',edgecolors='r')
+    y_init = ax.get_ylim()
+    x_init = ax.get_xlim()
 
-sim_data.apply(lambda x : plot_pie(x,ax), axis=1)
-ax.set_ylim(y_init)
-ax.set_xlim(x_init)
-plt.show()
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
+    sim_data.apply(lambda x : plot_pie(x,ax), axis=1)
+    ax.set_ylim(y_init)
+    ax.set_xlim(x_init)
+    plt.show()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print('Running main demo')
+    scatter_pie_plots()
