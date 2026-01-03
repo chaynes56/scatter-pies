0ee3e43f517b37d4c7e7b2163d94c3836112e383
Chris Haynes <75630580+chaynes56@users.noreply.github.com>
2026-01-03 17:59:11 -0600

    integrated detached HEAD work in spie.py and README.md, and added run/debug config

--- app/spie.py
+++ app/spie.py
@@ -1,51 +1,68 @@
-# Generate scatter plot of pie charts of precinct data in csv file
-# Assumes header row with format: PRECINCT,AttGen %,Turnout %,hs_vacp,hs_ownp,hs_renp,hs_vctp
-
-# To access data file: data_file_path = os.path.join(os.path.dirname(__file__), 'data/FILE.csv')
-# per https://pynsist.readthedocs.io/en/latest/faq.html#building-on-other-platforms
-
+#!/usr/bin/env python3
+"""
+Generate a scatter plot of pie charts of precinct data in csv file.
+Assumes header row with format: PRECINCT,AttGen %,Turnout %,hs_vacp,hs_ownp,hs_renp,hs_vctp.
+"""
 import matplotlib.pyplot as plt
 import csv
-import argparse
+import os
+from argparse import ArgumentParser
 
 # pie chart colors
-colors = ['green', 'red', 'yellow', 'blue'] # also 'magenta', 'cyan'
+colors = ['green', 'red', 'yellow', 'blue'] # others include 'magenta', 'cyan'
 color_key = ['hs_vacp', 'hs_ownp', 'hs_renp', 'hs_vctp']
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--csv_file", type=str, default = "../data/BL-data.csv",
-                    help="The name of a CSV precinct data file).")
-# Optional argument example:
-# parser.add_argument("--verbose", "-v", action="store_true", help="Enable verbose output (optional flag).")
-
+# command line argument parsing
+parser = ArgumentParser(prog='spie.py', epilog=__doc__)
+parser.add_argument('csv_file', type=str, default = '../data/BL-data.csv',
+                    help='The name of a precinct CSV file.')
+parser.add_argument('-i', '--image', type=str,
+                    help='The name of a file in which to save the plot figure.')
 args = parser.parse_args()
 
 def scatter_pie_plots(data) -> None :
-    # data is a list of row lists
+    """Generate a scatter plot of pie charts of precinct data in csv file.
+    :param data: a list of row lists
+    """
     data = data[1:] # remove header row
     data = [[row[0]] + [float(x) for x in row[1:]] for row in data] # float all but the precinct name
 
     transposed_data = [list(row) for row in zip(*data)] # a list of columns
-    precinct, x, y = transposed_data[:3]
+    precinct, x_values, y_values = transposed_data[:3]
 
     fig, ax = plt.subplots()
-    plt.scatter(x, y) # s=1000, facecolor='none',edgecolors='r' ??
-    y_init = ax.get_ylim()
+    plt.scatter(x_values, y_values)
     x_init = ax.get_xlim()
+    y_init = ax.get_ylim()
 
     for row in data:
-        ax.pie(row[3:], center=(row[1], row[2]), radius=0.02, colors=colors, frame=True)
+        precinct, x, y = row[0:3]
+        pie_values = row[3:]
+        ax.pie(pie_values, center=(x, y), radius=0.02, colors=colors, frame=True)
+        ax.text(x, y, str(precinct), ha='left', va='top', fontsize=6, color='black')
 
     ax.set_ylim(y_init)
     ax.set_xlim(x_init)
     ax.grid(True)
     plt.xlabel('Dems %')
     plt.ylabel('Turnout %')
-    ## plt.axis('on')
+
+    base = os.path.basename(args.csv_file)
+    file_name, _ = os.path.splitext(base)
+
+    # add pie chart color legend and title
+    text_legend = ''
+    for c, key in zip(colors, color_key):
+        text_legend += c + ': ' + key + '\n'
+    plt.figtext(0.7, 0.5, text_legend)
+    plt.figtext(0.4, 0.9, 'Precincts of ' + file_name, size=12)
+
+    if args.image:
+        plt.savefig(args.image, bbox_inches='tight', dpi=300)
     plt.show()
 
 if __name__ == '__main__':
-    print('Generating scatter plot of pie charts for data in ', args.csv_file)
+    print('Generating scatter plot of pie charts for data in', args.csv_file)
     with open(args.csv_file, newline='') as csvfile:
         csv_data = list(csv.reader(csvfile))
     scatter_pie_plots(csv_data)
